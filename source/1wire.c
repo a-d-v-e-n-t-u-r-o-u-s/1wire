ac07f5da603f5704b8292af4fafe579539a60312
Dawid Babula <dbabula@adventurous.pl>
2021-07-28 09:43:40 +0200

    Remove magic numbers
    Add/modify slot recovery time, please refer to the datasheet

--- source/1wire.c
+++ source/1wire.c
@@ -24,23 +24,39 @@
 #include "1wire.h"
 #include "hardware.h"
 #include <stddef.h>
+#include <limits.h>
 #include <util/delay.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+
+#define MASTER_TX_RESET_PULSE_TIME          (500u)
+
+/*
+ * TODO(DB) setting this time to 45us can cause that sensor won't be discovered
+ * because according to the documentation it has time till 60u, think about setting
+ * this time to 60uS
+ */
+#define MASTER_WAIT_FOR_PRESENCE            (45u)
+#define MASTER_WAIT_POST_PRESENCE           (470u)
+#define MASTER_TX_SLOT_INIT_TIME            (5u)
+#define MASTER_TX_HOLD_FOR_SAMPLING         (80u)
+#define MASTER_SLOT_RECOVERY_TIME           (2u)
+#define MASTER_RX_SLOT_INIT_TIME            (2u)
+#define MASTER_RX_WAIT_TO_SAMPLE            (15u)
 
 static void send_bit(bool bit)
 {
     cli();
 
     GPIO_config_pin(GPIO_CHANNEL_1WIRE, GPIO_OUTPUT_PUSH_PULL);
-    _delay_us(5);
+    _delay_us(MASTER_TX_SLOT_INIT_TIME);
 
     if(bit)
     {
         GPIO_config_pin(GPIO_CHANNEL_1WIRE, GPIO_INPUT_FLOATING);
     }
 
-    _delay_us(80);
+    _delay_us(MASTER_TX_HOLD_FOR_SAMPLING);
     GPIO_config_pin(GPIO_CHANNEL_1WIRE, GPIO_INPUT_FLOATING);
     sei();
 }
@@ -50,9 +66,9 @@ static bool read_bit(void)
     bool ret = false;
     cli();
     GPIO_config_pin(GPIO_CHANNEL_1WIRE, GPIO_OUTPUT_PUSH_PULL);
-    _delay_us(2);
+    _delay_us(MASTER_RX_SLOT_INIT_TIME);
     GPIO_config_pin(GPIO_CHANNEL_1WIRE, GPIO_INPUT_FLOATING);
-    _delay_us(15);
+    _delay_us(MASTER_RX_WAIT_TO_SAMPLE);
 
     ret = GPIO_read_pin(GPIO_CHANNEL_1WIRE);
     sei();
@@ -64,14 +80,14 @@ bool WIRE_reset(void)
     bool ret = false;
 
     GPIO_config_pin(GPIO_CHANNEL_1WIRE, GPIO_OUTPUT_PUSH_PULL);
-    _delay_us(500);
+    _delay_us(MASTER_TX_RESET_PULSE_TIME);
 
     GPIO_config_pin(GPIO_CHANNEL_1WIRE, GPIO_INPUT_FLOATING);
-    _delay_us(45);
+    _delay_us(MASTER_WAIT_FOR_PRESENCE);
 
     ret = !GPIO_read_pin(GPIO_CHANNEL_1WIRE);
 
-    _delay_us(470);
+    _delay_us(MASTER_WAIT_POST_PRESENCE);
 
     return ret;
 }
@@ -80,25 +96,27 @@ void WIRE_send_byte(uint8_t byte)
 {
     uint8_t tmp = byte;
 
-    for(uint8_t i = 0U; i < 8U; i++)
+    for(uint8_t i = 0U; i < CHAR_BIT; i++)
     {
         const bool bit = ((tmp & 0x01U) == 0x01);
         send_bit(bit);
         tmp >>= 1U;
+        _delay_us(MASTER_SLOT_RECOVERY_TIME);
     }
 }
 
 uint8_t WIRE_read_byte(void)
 {
     uint8_t ret = 0U;
-    for(uint8_t i = 0U; i < 8U ; i++)
+
+    for(uint8_t i = 0U; i < CHAR_BIT ; i++)
     {
         const bool bit = read_bit();
         if(bit)
         {
             ret |= (1U << i);
         }
-        _delay_us(15);
+        _delay_us(MASTER_SLOT_RECOVERY_TIME);
     }
     return ret;
 }
